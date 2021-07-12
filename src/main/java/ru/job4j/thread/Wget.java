5d04170249b12ee2844d4d0afb834dc8066feea5
MikhailPushkarev25 <mixa199525@yandex.ru>
2021-07-12 20:27:16 +0300

    4. Скачивание файла с ограничением. [#144271 #176652]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -13,10 +13,12 @@ import java.net.URL;
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
+    private final String nameFile;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String nameFile) {
         this.url = url;
         this.speed = speed;
+        this.nameFile = nameFile;
     }
 
     /**
@@ -27,13 +29,13 @@ public class Wget implements Runnable {
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-            FileOutputStream fileOutputStream = new FileOutputStream("pom_temp.xml")) {
+            FileOutputStream fileOutputStream = new FileOutputStream(nameFile)) {
             byte[] dataBuffer = new byte[speed];
             int bytesRead;
             long start = System.currentTimeMillis();
-            long finish = System.currentTimeMillis() - start;
             while ((bytesRead = in.read(dataBuffer, 0, speed)) != -1) {
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
+                long finish = System.currentTimeMillis() - start;
                 if (finish < speed) {
                     Thread.sleep(finish - speed);
                 }
@@ -44,9 +46,13 @@ public class Wget implements Runnable {
     }
 
     public static void main(String[] args) throws InterruptedException {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Not Argument!");
+        }
         String url = args[0];
         int speed =  Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        String nameFile = args[2];
+        Thread wget = new Thread(new Wget(url, speed, nameFile));
         wget.start();
         wget.join();
     }
