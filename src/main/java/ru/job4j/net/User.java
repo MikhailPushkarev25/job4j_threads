022791e29b5b316db2fefc683107a584af1c4cfa
MikhailPushkarev25 <mixa199525@yandex.ru>
2021-07-13 20:49:57 +0300

    3. Класс хранилища пользователей UserStorage [#1104]

---
+++ src/main/java/ru/job4j/net/User.java
@@ -0,0 +1,47 @@
+package ru.job4j.net;
+
+import java.util.Objects;
+
+public class User {
+    private volatile int id;
+    private volatile int amount;
+
+    public User(int id, int amount) {
+        this.id = id;
+        this.amount = amount;
+    }
+
+    public synchronized int getId() {
+        return id;
+    }
+
+    public synchronized int getAmount() {
+        return amount;
+    }
+
+    public synchronized void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+
+    public static synchronized User of(User user) {
+        return new User(user.id, user.amount);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return id == user.id && amount == user.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, amount);
+    }
+}
---
+++ src/main/java/ru/job4j/net/UserStorage.java
@@ -0,0 +1,53 @@
+package ru.job4j.net;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ThreadSafe
+public class UserStorage {
+
+    @GuardedBy("this")
+    private final Map<Integer, User> map = new HashMap<>();
+
+    public synchronized boolean add(User user) {
+        return map.putIfAbsent(user.getId(), User.of(user)) == null;
+    }
+
+    public synchronized boolean update(User user) {
+        return map.replace(user.getId(), User.of(user)) != null;
+    }
+
+    public synchronized boolean delete(User user) {
+        return map.remove(user.getId(), User.of(user));
+    }
+
+    public synchronized boolean transfer(int fromId, int told, int amount) {
+        boolean rsl = true;
+        User form = map.get(fromId);
+        User to = map.get(told);
+
+        if (form == null || to == null || form.getAmount() < amount) {
+            rsl = false;
+        }
+
+        form.setAmount(form.getAmount() - amount);
+
+        to.setAmount(to.getAmount() + amount);
+
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        UserStorage userStorage = new UserStorage();
+        System.out.println(userStorage.add(new User(1, 100)));
+        System.out.println(userStorage.add(new User(2, 200)));
+        System.out.println();
+       // System.out.println(userStorage.update(new User(1, 500)));
+        System.out.println();
+        //System.out.println(userStorage.delete(new User(1, 500)));
+        System.out.println(userStorage.transfer(1, 2, 50));
+    }
+}
