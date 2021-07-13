eed10c8225b29902ce5816a1a28fbd0f9390e8f6
MikhailPushkarev25 <mixa199525@yandex.ru>
2021-07-13 06:04:28 +0300

    2. JCIP. Настройка библиотеки [#268575]

--- src/main/java/ru/job4j/concurrent/Count.java
+++ src/main/java/ru/job4j/concurrent/Count.java
@@ -1,6 +1,11 @@
 package ru.job4j.concurrent;
 
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
 public class Count {
+    @GuardedBy("this")
     private int value;
 
     public synchronized void increment() {
---
+++ src/test/java/ru/job4j/concurrent/CountTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.concurrent;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CountTest {
+
+    /**
+     * Класс описывает нить со счетчиком
+     */
+
+    private class ThreadCount extends Thread {
+        private final Count count;
+
+        public ThreadCount(final Count count) {
+            this.count = count;
+        }
+
+        @Override
+        public void run() {
+            this.count.increment();
+        }
+    }
+
+    @Test
+    public void whenExecute2ThreadThen2() throws InterruptedException {
+        /* Создаем счетчик*/
+        Count count = new Count();
+        /* Создаем нити */
+        Thread first = new ThreadCount(count);
+        Thread second = new ThreadCount(count);
+        /* Запускаем нити */
+        first.start();
+        second.start();
+        /* заставляем главную нить дождаться выполнения главных нитей */
+        first.join();
+        second.join();
+        assertThat(count.get(), is(2));
+    }
+}
