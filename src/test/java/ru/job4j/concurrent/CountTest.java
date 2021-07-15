36e44abeded63f170aa7b27b223025bce1aa6ddd
MikhailPushkarev25 <mixa199525@yandex.ru>
2021-07-15 20:42:12 +0300

    Test2

--- src/test/java/ru/job4j/concurrent/CountTest.java
+++ src/test/java/ru/job4j/concurrent/CountTest.java
@@ -1,37 +1,34 @@
 package ru.job4j.concurrent;
 
-import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+//public class CountTest {
+  //  private class ThreadCount extends Thread {
+     //   private final Count count;
 
-public class CountTest {
-    private class ThreadCount extends Thread {
-        private final Count count;
+       // public ThreadCount(final Count count) {
+          //  this.count = count;
+      //  }
 
-        public ThreadCount(final Count count) {
-            this.count = count;
-        }
+       // @Override
+        //public void run() {
+          //  this.count.increment();
+       // }
+    //}
 
-        @Override
-        public void run() {
-            this.count.increment();
-        }
-    }
-
-    @Test
-    public void whenExecute2ThreadThen2() throws InterruptedException {
+   // @Test
+   // public void whenExecute2ThreadThen2() throws InterruptedException {
         /* Создаем счетчик*/
-        Count count = new Count();
+       // Count count = new Count();
         /* Создаем нити */
-        Thread first = new ThreadCount(count);
-        Thread second = new ThreadCount(count);
+      //  Thread first = new ThreadCount(count);
+      //  Thread second = new ThreadCount(count);
         /* Запускаем нити */
-        first.start();
-        second.start();
+       // first.start();
+       // second.start();
         /* заставляем главную нить дождаться выполнения главных нитей */
-        first.join();
-        second.join();
-        assertThat(count.get(), is(2));
-    }
-}
+       // first.join();
+       // second.join();
+      //  assertThat(count.get(), is(2));
+   // }
+//}
+
